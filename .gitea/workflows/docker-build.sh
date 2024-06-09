dd1ca8a6c6b1c5dc3234c2a2170f0d6383ea3829
Jiangyj.Tech <Chief@Jiangyj.Tech>
2024-06-09 21:08:10 +0800

    测试 docker build

--- .gitea/workflows/docker-build.sh
+++ .gitea/workflows/docker-build.sh
@@ -2,4 +2,5 @@ docker build -t filesys .
 # 获取当前的日时分秒
 datetime=$(date "+%d%H%M")
 docker tag filesys csjiangyj/filesys:$datetime
-docker push csjiangyj/filesys:$datetime
+docker tag csjiangyj/filesys:$datetime csjiangyj/filesys:latest
+docker push csjiangyj/filesys:latetest
