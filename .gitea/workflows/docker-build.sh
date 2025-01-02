3d9516063832aaad0afac3fa301f4047f88fa969
JiangYinjin <i@JYJ.cx>
2025-01-02 17:30:04 +0800

    Comment out Docker build commands in workflow script for clarity and future reference

--- .gitea/workflows/docker-build.sh
+++ .gitea/workflows/docker-build.sh
@@ -1,7 +1,7 @@
-cd /root/proj/filesys/current
-docker build -t filesys .
-# 获取当前的日时分秒
-datetime=$(date "+%d%H%M")
-docker tag filesys csjiangyj/filesys:$datetime
-docker tag csjiangyj/filesys:$datetime csjiangyj/filesys:latest
-docker push csjiangyj/filesys:latest
+# cd /root/proj/filesys
+# docker build -t filesys .
+# # 获取当前的日时分秒
+# datetime=$(date "+%d%H%M")
+# docker tag filesys csjiangyj/filesys:$datetime
+# docker tag csjiangyj/filesys:$datetime csjiangyj/filesys:latest
+# docker push csjiangyj/filesys:latest
